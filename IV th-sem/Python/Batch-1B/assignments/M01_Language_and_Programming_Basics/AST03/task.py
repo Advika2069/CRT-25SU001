ba87454f156f353be71cb16f660a94b1204b9dff
Advika2069 <advikakanchanapally@gmail.com>
2026-02-19 16:33:12 +0530

    Update task.py

--- IV th-sem/Python/Batch-1B/assignments/M01_Language_and_Programming_Basics/AST03/task.py
+++ IV th-sem/Python/Batch-1B/assignments/M01_Language_and_Programming_Basics/AST03/task.py
@@ -1,8 +1,12 @@
-def Student_Grade_System(name:str,n1: int,n2: int,n3: int) -> str:
-   pass
+def Student_Grade_System(name, g1, g2, g3):
+    average = (g1 + g2 + g3) / 3
 
+    # Truncate to 2 decimal places (not round)
+    average = int(average * 100) / 100
 
-if __name__ == '__main__':
-    name = input()
-    n1,n2,n3 = list(map(int,input().split()))
-    print(Student_Grade_System(name,n1,n2,n3))
+    if average >= 40:
+        status = "Pass"
+    else:
+        status = "fail"
+
+    return f"Average grade: {average}, Status: {status}"
